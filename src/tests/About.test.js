3fffc35576b122f9bfcaefedb334eaedec7cb756
Rita Moura <ritamoura520@gmail.com>
2022-11-22 17:05:52 -0300

    Feat: Implementado testes para página Favorite Pokémon
    Foi verificado se continha na tela a mensagem "no favorite pokémon found" caso não tivesse senhum pokémon favoritado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { About } from '../pages';
 
-describe('Testa se a página contém as informações sobre a Pokédex', () => {
-  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+describe('Testa se a página contém as informações sobre a Pokédex.', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
     render(<About />);
 
     const aboutTitle = screen.getByRole(
@@ -15,7 +15,7 @@ describe('Testa se a página contém as informações sobre a Pokédex', () => {
     expect(aboutTitle).toBeInTheDocument();
   });
 
-  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
     render(<About />);
 
     const oneParagraph = screen.getByText(
@@ -27,7 +27,7 @@ describe('Testa se a página contém as informações sobre a Pokédex', () => {
     expect(twoParagraph).toBeInTheDocument();
   });
 
-  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  test('Testa se a página contém a seguinte imagem de uma Pokédex.', () => {
     render(<About />);
 
     const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemon } from '../pages';
+
+describe('Testa se as informações corretas são exibidas na página Favorite pokémon.', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
+    render(<FavoritePokemon />);
+
+    const erroMessage = screen.getByText(/no favorite pokémon found/i);
+    expect(erroMessage).toBeInTheDocument();
+  });
+});
