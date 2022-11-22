65add7e783037f48062f1b28b091c744feb47bc0
Rita Moura <ritamoura520@gmail.com>
2022-11-22 17:16:08 -0300

    Feat: Implementado testes para página NotFound
    Foi testado se a página contém um h2 com o texto correto e se comtém a imagem correta

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { FavoritePokemon } from '../pages';
 
 describe('Testa se as informações corretas são exibidas na página Favorite pokémon.', () => {
-  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
+  test('Testa se é exibido na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
     render(<FavoritePokemon />);
 
-    const erroMessage = screen.getByText(/no favorite pokémon found/i);
+    const erroMessage = screen.getByText('No favorite Pokémon found');
     expect(erroMessage).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+
+describe('Testa se a página notFound contém as informações corretas', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found',
+        level: 2,
+      },
+    );
+    expect(title).toBeInTheDocument();
+  });
+  test('Testa se a página mostra a imagem', () => {
+    render(<NotFound />);
+
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const img = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(URL);
+  });
+});
