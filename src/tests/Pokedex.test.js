66e2e4f6aa671945f2d34999a886ae3422e3175c
Rita Moura <ritamoura520@gmail.com>
2022-11-23 09:17:56 -0300

    Feat: Implementa testes na página Pokedex
    Foi testado se os botões da tela continha o texto correto e se realizavam sua função corretamente, também foi verificado se todas as informações renderizadas estavam corretas

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,9 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
+import pokemonList from '../data';
+import { Pokedex } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Testa se a página Pokedex renderiza as informações corretas.', () => {
@@ -9,12 +12,101 @@ describe('Testa se a página Pokedex renderiza as informações corretas.', () =
 
     const title = screen.getByRole('heading', {
       name: 'Encountered Pokémon',
+      level: 2,
     });
     expect(title).toBeInTheDocument();
   });
-  // test('Testa se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado.', () => {
-  //   renderWithRouter(<App />);
+  test('Testa se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ { null: null } }
+      />,
+    );
 
-  //   const allButton = screen.getAllByTestId('pokemon-type-button');
-  // });
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    buttons.forEach((button) => {
+      expect(button).toBeInTheDocument();
+    });
+
+    const eletric = screen.getByRole('button', {
+      name: 'Electric',
+    });
+    expect(eletric).toBeInTheDocument();
+
+    const fire = screen.getByRole('button', {
+      name: 'Fire',
+    });
+    expect(fire).toBeInTheDocument();
+
+    const bug = screen.getByRole('button', {
+      name: 'Bug',
+    });
+    expect(bug).toBeInTheDocument();
+
+    const poison = screen.getByRole('button', {
+      name: 'Poison',
+    });
+    expect(poison).toBeInTheDocument();
+
+    const psychic = screen.getByRole('button', {
+      name: 'Psychic',
+    });
+    expect(psychic).toBeInTheDocument();
+
+    const normal = screen.getByRole('button', {
+      name: 'Normal',
+    });
+    expect(normal).toBeInTheDocument();
+
+    const dragon = screen.getByRole('button', {
+      name: 'Dragon',
+    });
+    expect(dragon).toBeInTheDocument();
+  });
+  test('Testa se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ { null: null } }
+      />,
+    );
+
+    const nextButton = screen.getByRole('button', {
+      name: 'Próximo Pokémon',
+    });
+    expect(nextButton).toBeInTheDocument();
+    userEvent.click(nextButton);
+    const pokemon = screen.getByText('Charmander');
+    expect(pokemon).toBeInTheDocument();
+  });
+  test('Testa se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ { null: null } }
+      />,
+    );
+    const imgPokemon = screen.getAllByRole('img', {
+      name: /sprite/i,
+    });
+    expect(imgPokemon).toHaveLength(1);
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro.', () => {
+    renderWithRouter(
+      <Pokedex
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ { null: null } }
+      />,
+    );
+
+    const buttonAll = screen.getByRole('button', {
+      name: 'All',
+    });
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(buttonAll);
+    const onePokemon = screen.getByText('Pikachu');
+    expect(onePokemon).toBeInTheDocument();
+  });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+// import React from "react";
+
+// describe('Testa se a página Pokemon details renderiza as informções corretas.', () => {
+//   test('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
+
+//   })
+// })
